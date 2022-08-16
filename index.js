98785d714ba3fb66a66cdd28b7ad347f542fde81
Maureenkamau03 <mkaymaureen@gmail.com>
2022-08-17 01:51:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  if (ride > 2500) {
+    return "No can do.";
+  } else if (ride > 2000) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "This one is on me!";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let cityValue = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return cityValue;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
